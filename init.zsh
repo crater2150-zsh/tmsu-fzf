1051e5658b2c655845c4739bbb73e6564f6e182e
Alexander Gehrke <github@qwertyuiop.de>
2020-01-20 11:15:48 +0100

    Initial commit

---
+++ init.zsh
@@ -0,0 +1,19 @@
+export TMSU_FZF_ROOT="$0:a:h"
+
+: "Autoload functions and Create widgets" && () {
+  local dir="$TMSU_FZF_ROOT/autoload"
+  fpath=($dir/**/*(N-/) $fpath)
+
+  autoload -Uz `ls -F $dir/**/* | grep -v /`
+
+  local w
+  for w in `ls $dir/widgets/`; do zle -N $w; done
+}
+
+# Support zsh-autosuggestions
+if [[ -n ZSH_AUTOSUGGEST_IGNORE_WIDGETS ]]; then
+  ZSH_AUTOSUGGEST_IGNORE_WIDGETS=(
+    $ZSH_AUTOSUGGEST_IGNORE_WIDGETS
+    `ls $TMSU_FZF_ROOT/autoload/widgets/`
+  )
+fi
